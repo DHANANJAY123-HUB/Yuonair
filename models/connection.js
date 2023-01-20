f2ddcd4c0ba1a5f33f1e047765be727b132a8bb8
DHANANJAY123-HUB <75534106+DHANANJAY123-HUB@users.noreply.github.com>
2023-01-20 15:37:10 +0530

    commit

--- models/connection.js
+++ models/connection.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.set('strictQuery',true);
-const url = "mongodb://localhost:27017/Yuonair"
-mongoose.connect(url)
+//const url = "mongodb://localhost:27017/Yuonair"
+mongoose.connect('mongodb+srv://user:user@cluster0.lcji4ad.mongodb.net/Yuonair?retryWrites=true&w=majority',{
+  useNewUrlParser:true,
+  useUnifiedTopology:true
+})
+//mongoose.connect(url)
 const db = mongoose.connection
 console.log("Successfully connected to mongodb database")
 module.exports = db
